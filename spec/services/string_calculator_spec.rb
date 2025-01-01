f8045339c5da0159d1e6f532d389af17faae4a50
AnkitNagar12 <yuvrajd@witmates.com>
2025-01-01 21:31:50 +0530

    2nd commit: Add Single Number

--- spec/services/string_calculator_spec.rb
+++ spec/services/string_calculator_spec.rb
@@ -6,5 +6,11 @@ RSpec.describe StringCalculator, type: :service do
       calculator = StringCalculator.new
       expect(calculator.add('')).to eq(0)
     end
+
+    it 'returns the number itself for a single number' do
+      calculator = StringCalculator.new
+      expect(calculator.add('1')).to eq(1)
+    end
+
   end
 end
